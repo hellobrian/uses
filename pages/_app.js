305fef490139747c26f3264b0599c081400d17a9
hellobrian <brianhan@hey.com>
2020-08-07 01:37:44 -0500

    👌 IMPROVE: cleanup commented code

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,6 @@
 import React from "react";
-// import NextApp from "next/app";
 import { ThemeProvider } from "theme-ui";
-import theme from "../utils/theme";
-// import "@/styles/globals.css";
-
-// export default class App extends NextApp {
-//   render() {
-//     const { Component, pageProps } = this.props;
-//     return (
-//       <ThemeProvider theme={theme}>
-//         <Component {...pageProps} />
-//       </ThemeProvider>
-//     );
-//   }
-// }
+import theme from "@utils/theme";
 
 const App = ({ Component, pageProps }) => (
   <ThemeProvider theme={theme}>
